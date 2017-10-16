5aeb76efe5f6078284848663d927bb4b672f1d7c
Sergey Lazarev <serega486@gmail.com>
2017-10-16 22:36:54 +0300

    try to get file

--- MammothZinoviyBot/src/main/java/Chords.java
+++ MammothZinoviyBot/src/main/java/Chords.java
@@ -1,4 +1,4 @@
-import java.io.File;
+import java.io.*;
 
 /**
  * Created by sereg on 16.09.2017.
@@ -6,7 +6,18 @@ import java.io.File;
 public class Chords {
     public static String getChords(String substring) {
         String[] songData = Utils.getSongData(substring);
+        String msg = new String();
+        try {
+            FileReader fileReader = new FileReader(Chords.class.getResource(songData[0].substring(0,0) + "/" +
+                    songData[0] + "/" + songData[1] + "/chords.txt").getPath());
+            BufferedReader bufferedReader = new BufferedReader(fileReader);
+            msg = bufferedReader.readLine();
+            bufferedReader.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+            return "Ooops! Что-то пошло не так! ПОМОГИТЕ!";
+        }
 //        File chords = new File();
-        return songData.toString();
+        return msg;
     }
 }
