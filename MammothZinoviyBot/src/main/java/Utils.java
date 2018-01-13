4646fc52dd4441cfce65a4f2041c38cf3deaaab4
Sergey Lazarev <serega486@gmail.com>
2018-01-13 20:01:34 +0300

    caching songs

--- MammothZinoviyBot/src/main/java/Utils.java
+++ MammothZinoviyBot/src/main/java/Utils.java
@@ -1,7 +1,8 @@
 public class Utils {
     public static String[] getSongData(String request){
-        String singer = request.substring(0, request.indexOf("—") - 1);
-        String song = request.substring(request.indexOf("—") + 2);
+        String singer = request.substring(0, request.indexOf('—'));
+        String song = request.substring(request.indexOf('—') + 2);
+        singer = singer.substring(0, singer.length()-1);
         System.out.println("Singer: " + singer);
         System.out.println("Song: " + song);
         String[] songData = {singer, song};
