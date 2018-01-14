b257065c3ec9999b3213efa14f610f2f08027806
Sergey Lazarev <serega486@gmail.com>
2018-01-14 21:51:38 +0300

    Revert "merge"
    This reverts commit c32573c1aeca830815f0571178d8c8ec58ac9b06.

--- MammothZinoviyBot/src/main/java/Texts.java
+++ MammothZinoviyBot/src/main/java/Texts.java
@@ -1,10 +1,35 @@
+import org.apache.commons.io.IOUtils;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.HashMap;
+
 /**
  * Created by sereg on 16.09.2017.
  */
 public class Texts {
 
+    static HashMap<String, String> texts = new HashMap<>();
+
     public static String getText(String substring) {
-        String[] songData = Utils.getSongData(substring);
-        return songData.toString();
+        String msg = null;
+        if (texts.get(substring) == null) {
+            String[] songData = Utils.getSongData(substring);
+            try {
+                String path = songData[0].substring(0, 1) + "/" + songData[0] + "/" + songData[1] + "/text.txt";
+                System.out.println(path);
+                ClassLoader classLoader = ClassLoader.getSystemClassLoader();
+                File file = new File(classLoader.getResource(path).getFile());
+                msg = IOUtils.toString(classLoader.getResourceAsStream(path));
+                if (msg == null || msg.isEmpty()) return "Файл " + substring + " не найден!";
+            } catch (IOException e) {
+                e.printStackTrace();
+                return "Ooops! Что-то пошло не так! ПОМОГИТЕ!";
+            }
+            texts.put(substring, msg);
+        } else {
+            msg = texts.get(substring);
+        }
+        return msg;
     }
 }
